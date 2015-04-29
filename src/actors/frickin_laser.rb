2957909e5f4683c44871d6c5d0dcfafbacace363
zotherstupidguy <zotherstupidguy@gmail.com>
2015-04-29 16:36:14 +0300

    adds graphical view to freakin_laser

--- src/actors/frickin_laser.rb
+++ src/actors/frickin_laser.rb
@@ -3,6 +3,7 @@ define_actor :frickin_laser do
   #has_attributes view: :graphical_actor_view
 
   has_behaviors do
+    graphical
     #animated          frame_update_time: 900
     in_bounds_or_death bounds: [0,30,:viewport_width,:viewport_height]
     projectile         speed: 0.9, direction: vec2(0, -1)
@@ -11,10 +12,10 @@ define_actor :frickin_laser do
     collidable         shape: :circle, :radius => 40
   end
 
-  view do
-    draw do |target, x_offset, y_offset, z|
-      #target.fill(actor.x, actor.y, actor.x+actor.radius/2.0, actor.y-actor.radius*2, [0xff, 0xff, 0xff], z)
-      target.fill(actor.x, actor.y, actor.x+actor.radius/2.0, actor.y-actor.radius*2, [0xaa, 0xaa, 0xaa], z)
-    end
-  end
+  #view do
+  #  draw do |target, x_offset, y_offset, z|
+  #    #target.fill(actor.x, actor.y, actor.x+actor.radius/2.0, actor.y-actor.radius*2, [0xff, 0xff, 0xff], z)
+  #    target.fill(actor.x, actor.y, actor.x+actor.radius/2.0, actor.y-actor.radius*2, [0xaa, 0xaa, 0xaa], z)
+  #  end
+  #end
 end
